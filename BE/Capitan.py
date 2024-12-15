69f6caa6ec31bcb11d866167ab5436f1c0c07f88
Mallid <mallidbernardodagil@gmail.com>
2024-12-15 16:09:30 -0300

    se agrego datos a la clase Capitan y tambien se agrego una linea mas de respuesta al incio

---
+++ BE/Capitan.py
@@ -0,0 +1,6 @@
+class capitan:
+    def __init__(self, nombre, apellido, dni):
+        self.Nombre = nombre
+        self.apellido = apellido
+        self.Dni = dni
+        
--- Inicio.py
+++ Inicio.py
@@ -1,1 +1,12 @@
-Hola = print("Hola como estas")
+
+def Ejecutar():
+    Hola = print("Hola como estas")
+
+    respuesta = print("Bien y vos")
+
+    respuesta1 = print("esta noche se sale")
+
+
+
+if __name__ == "__main__":
+    Ejecutar()
